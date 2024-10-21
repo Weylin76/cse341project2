1055f16854557bc4f75fc576aeee228a230e74a8
Weylin76 <Weylin76@msn.com>
2024-10-20 22:01:59 -0400

    updated auth function

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,7 +1,10 @@
-// authMiddleware.js
-module.exports.isLoggedIn = (req, res, next) => {
+// Example isLoggedIn middleware for more debug
+function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
+        console.log('User is authenticated:', req.user);
         return next();
+    } else {
+        console.log('User is not authenticated, redirecting to home');
+        res.redirect('/');
     }
-    res.redirect('/');
-};
+}
